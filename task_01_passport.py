638bcff8381d364e744ee60e192aa99e170edda4
skeletoni97 <Usanov.Artem.Ra@yandex.ru>
2026-04-06 19:46:54 +0300

    create passport lukoil arena

---
+++ task_01_passport.py
@@ -0,0 +1,21 @@
+student_name = "Усанов Артем Сергеевич"
+group_number = "3140801/52501"
+project_name = "Лукойл Арена"
+floors = 5
+height = 52.0
+is_residential = False
+construction_year = 2014
+
+print("=== ПАСАОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА ===")
+print(f"Составитель: {student_name}")
+print(f"Группа: {group_number}")
+print()
+print(f"Название объекта: {project_name}")
+print(f"Этажность: {floors}")
+print(f"Высота: {height} м")
+print(f"Тип: {'Жилой' if is_residential else 'Нежилое'}")
+print(f"Год постройки: {construction_year}")
+
+# Комментарии:
+# Объект находится по адресу: Москва, Волоколамское шоссе, 69.
+# Я выбрал этот объект, потому что много работал на нем и хорошо знаком с его особенностями.
