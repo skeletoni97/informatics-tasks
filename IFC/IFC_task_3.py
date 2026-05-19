d2427768a8face3772c2701e3cbe63dfd0f1e2d0
skeletoni97 <Usanov.Artem.Ra@yandex.ru>
2026-05-19 23:18:50 +0300

    добавил вывод Property Sets первой стены IFC

---
+++ IFC/IFC_task_3.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import ifcopenshell
+import ifcopenshell.util
+import ifcopenshell.util.element
+from pathlib import Path
+
+filepath = './IFC/Example_1.ifc'
+model = ifcopenshell.open(filepath)
+
+walls = model.by_type("IfcWall")
+
+if len(walls) > 0:
+    print(f"Количество стен в модели: {len(walls)}")
+    first_wall = walls[0]
+
+    psets = ifcopenshell.util.element.get_psets(first_wall)
+
+    print("Полный словарь наборов свойств:")
+    print(psets)
+
+    print("\nПеречень Property Sets первой стены:")
+
+    for pset_name, properties in psets.items():
+        print(f"\nPset: {pset_name}")
+
+        for property_name, property_value in properties.items():
+            print(f"{property_name}: {property_value}")
+
+else:
+    print("В модели стены не найдены.")
