bb321c44ebf28432e487ac614a251477ee5da388
skeletoni97 <Usanov.Artem.Ra@yandex.ru>
2026-05-19 22:53:58 +0300

    добавил скрипт для подсчета стен в IFC

--- IFC/IFC_task_1.py
+++ IFC/IFC_task_1.py
@@ -6,8 +6,10 @@ import ifcopenshell.util
 import ifcopenshell.util.element
 from pathlib import Path
 
-
-model = ifcopenshell.open('./IFC/Example_1.ifc')
+filepath = './IFC/Example_1.ifc'
+model = ifcopenshell.open(filepath)
 
 elements = model.by_type('ifcElement')
 print(len(elements))
+walls = model.by_type("IfcWall")
+print(f"Количество стен в модели: {len(walls)}")
