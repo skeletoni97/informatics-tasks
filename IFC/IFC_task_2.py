2b84cbb29dd7e136d2b9c4803e5f02f1a806547f
skeletoni97 <Usanov.Artem.Ra@yandex.ru>
2026-05-19 23:06:56 +0300

    добавил вывод информации о первой стене IFC

---
+++ IFC/IFC_task_2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import ifcopenshell
+import ifcopenshell.util
+import ifcopenshell.util.element
+from pathlib import Path
+
+filepath = './IFC/Example_1.ifc'
+model = ifcopenshell.open(filepath)
+
+walls = model.by_type("IfcWall")
+
+if len(walls) > 0:
+    print(f"Количество стен в модели: {len(walls)}")
+    first_wall = walls[0]
+
+    print("Информация о первой стене:")
+    print(f"  GlobalId: {first_wall.GlobalId}")
+    print(f"  Name: {first_wall.Name}")
+    print(f"  ObjectType: {first_wall.ObjectType}")
+else:
+    print("В модели стены не найдены.")
